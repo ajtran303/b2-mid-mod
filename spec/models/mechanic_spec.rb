1ff10b87662946edad0eea048ee39c57d81a8fee
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-17 11:22:27 -0600

    Add spec for method mechanic#rides_in_workload

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -13,4 +13,21 @@ RSpec.describe Mechanic do
     end
   end
 
+  describe "methods" do
+    kara = Mechanic.create!(name: "Kara Tran", years_of_experience: 11)
+    hershey_park = AmusementPark.create!(name: "Hershey Park", admission_price: 50)
+
+    frog_hopper = hershey_park.rides.create!(name: "The Frog Hopper", thrill_rating: 10)
+    fahrenheit = hershey_park.rides.create!(name: "Fahrenheit", thrill_rating: 5)
+    kiss_raise = hershey_park.rides.create!(name: "The Kiss Raise", thrill_rating: 8)
+
+    RideMechanic.create!(mechanic_id: kara.id, ride_id: frog_hopper.id)
+    RideMechanic.create!(mechanic_id: kara.id, ride_id: fahrenheit.id)
+    RideMechanic.create!(mechanic_id: kara.id, ride_id: kiss_raise.id)
+
+    it "#rides_in_workload" do
+      expect(kara.rides_in_workload).to eq(["The Frog Hopper", "Fahrenheit", "The Kiss Raise"])
+    end
+  end
+
 end
