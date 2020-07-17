50c7fec1303242bfb8d036594a0d6b6d32c65da8
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-17 11:47:54 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@ frog_hopper = hershey_park.rides.create!(name: "The Frog Hopper", thrill_rating:
 fahrenheit = hershey_park.rides.create!(name: "Fahrenheit", thrill_rating: 5)
 kiss_raise = hershey_park.rides.create!(name: "The Kiss Raise", thrill_rating: 8)
 
+RideMechanic.create!(mechanic_id: kara.id, ride_id: frog_hopper.id)
+RideMechanic.create!(mechanic_id: kara.id, ride_id: fahrenheit.id)
+RideMechanic.create!(mechanic_id: kara.id, ride_id: kiss_raise.id)
+
 great_bear = hershey_park.rides.create!(name: "The Great Bear", thrill_rating: 10)
