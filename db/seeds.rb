557e41a2852eb78b634ed768c9e3b5b1daff4782
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-17 11:45:47 -0600

    Add seeds for development environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+kara = Mechanic.create!(name: "Kara Tran", years_of_experience: 11)
+
+hershey_park = AmusementPark.create!(name: "Hershey Park", admission_price: 50)
+
+frog_hopper = hershey_park.rides.create!(name: "The Frog Hopper", thrill_rating: 10)
+fahrenheit = hershey_park.rides.create!(name: "Fahrenheit", thrill_rating: 5)
+kiss_raise = hershey_park.rides.create!(name: "The Kiss Raise", thrill_rating: 8)
+
+great_bear = hershey_park.rides.create!(name: "The Great Bear", thrill_rating: 10)
