4f1d77165272e2464e969978296894b025ce7698
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-17 10:02:27 -0600

    Fix setup for mechanic attributes spec

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -6,10 +6,7 @@ RSpec.describe Mechanic do
   end
 
   describe "attributes" do
-    bat_land = AmusementPark.create!(name: "Bat Land", admission_price: 45)
-    bat_mobile = bat_land.rides.create!(name: "The Bat-mobile", thrill_rating: 10)
     alfred = Mechanic.create!(name: "Alfred, the Butler", years_of_experience: 30)
-    # glove_world = AmusementPark.create!(name: "Glove World", admission_price: 30)
     it "has attributes" do
       expect(alfred.name).to eq("Alfred, the Butler")
       expect(alfred.years_of_experience).to eq(30)
